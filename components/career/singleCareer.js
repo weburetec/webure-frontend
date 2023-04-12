85ca045740b24f849b001b05d99adce00564f962
weburetec <58927451+weburetec@users.noreply.github.com>
2023-04-12 23:39:22 +0530

    Update singleCareer.js

--- components/career/singleCareer.js
+++ components/career/singleCareer.js
@@ -230,9 +230,15 @@ const SingleCareer = () => {
           </div>          
           <p className="text-[#676767] text-lg tracking-wide font-medium underline-offset-2 font-Lexend">WORKING HOURS</p>
           <div className="text-[#676767] text-base text-justify tracking-wide font-Lexend">
+            <div className={styles.time}>
               <button className="text-[#BE5151]">Full Time</button>
+            </div>
+            <div className={styles.time}>
               <button className="text-[#43A552]">Mid Level</button>
+            </div>
+            <div className={styles.time}>
               <button className="text-[#3E6EC3]">2-3 Years</button>
+            </div>
           </div>          
         </div>
         <Formik
