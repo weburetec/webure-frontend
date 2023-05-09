c31edd794cd283e0fa7cbc258bbb9d09f9a7f232
weburetec <58927451+weburetec@users.noreply.github.com>
2023-05-09 19:41:22 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -281,7 +281,7 @@ export default function Example() {
                 </>
                 );}}
               </Popover>
-              <div class="flex flex-col sm:flex-row justify-between items-center px-4 py-2">
+              <div class="flex flex-col sm:flex-row justify-between items-center py-2">
                 <div class="flex flex-col sm:flex-row items-center mt-4 sm:mt-0">
                   <a href="/hiretalent" class="font-Lexend text-base font-medium text-gray-500 hover:text-gray-900 mt-2 sm:mt-0 sm:mr-4">Technologies</a>
                 </div>
