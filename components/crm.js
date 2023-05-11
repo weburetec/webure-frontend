4c8d52286415e3096c17fe0da83115aac7b72dd5
weburetec <58927451+weburetec@users.noreply.github.com>
2023-05-11 22:59:33 +0530

    Update crm.js

--- components/crm.js
+++ components/crm.js
@@ -25,21 +25,21 @@ export default function CRM() {
                             <div class="w-2/4">
                                 <div class=" bg-white font-normal cursor-pointer rounded-lg border-2 border-gray-300 p-6 m-6 mx-auto max-w-md  ">
                                     <p class="font-Lexend text-lg font-semibold"><strong>Frequently Asked Questions</strong></p>
-                                    <p class="font-Lexend text-lg font-normal">Look through our Frequently Asked Questions to find the info you need.</p>
+                                    <p class="font-Lexend text-lg font-normal">Look through our Frequently Asked Questions to find the info you need.</p><span class="text-white text-sm font-Lexend bg-purple-800 px-2 py-1 rounded-md">Visit FAQs Page</span>
                                 </div>
                                 <div class=" bg-white font-normal cursor-pointer rounded-lg border-2 border-gray-300 p-6 m-6 mx-auto max-w-md  ">
                                     <p class="font-Lexend text-lg font-semibold"><strong>Call Us</strong></p>
-                                    <p class="font-Lexend text-lg font-normal">If you would like to talk to us in person, you can call us at<span class="text-white text-sm font-Lexend bg-purple-800 px-2 py-1 rounded-md">+91 803 786 2398</span></p>
+                                    <p class="font-Lexend text-lg font-normal">If you would like to talk to us in person, you can call us at</p><span class="text-white text-sm font-Lexend bg-purple-800 px-2 py-1 rounded-md">+91 803 786 2398</span>
                                 </div>
                             </div>
                             <div class="w-1/2">
                                 <div class=" bg-white font-normal cursor-pointer rounded-lg border-2 border-gray-300 p-6 m-6 mx-auto max-w-md  ">
                                     <p class="font-Lexend text-lg font-semibold"><strong>Chat with Us</strong></p>
-                                    <p class="font-Lexend text-lg font-normal">Got a question you need answered right away? We are here for that.<span class="text-white text-sm font-Lexend bg-purple-800 px-2 py-1 rounded-md">Click on Chat Now</span></p>
+                                    <p class="font-Lexend text-lg font-normal">Got a question you need answered right away? We are here for that.</p><span class="text-white text-sm font-Lexend bg-purple-800 px-2 py-1 rounded-md">Click on Chat Now</span>
                                 </div>
                                 <div class=" bg-white font-normal cursor-pointer rounded-lg border-2 border-gray-300 p-6 m-6 mx-auto max-w-md  ">
                                     <p class="font-Lexend text-lg font-semibold"><strong>Email Us</strong></p>
-                                    <p class="font-Lexend text-lg font-normal">You can simply send us an email. Our support staff will get back to you ASAP<span class="text-white text-sm font-Lexend bg-purple-800 px-2 py-1 rounded-md">hi@weburetech.com</span></p>
+                                    <p class="font-Lexend text-lg font-normal">You can simply send us an email. Our support staff will get back to you ASAP</p><span class="text-white text-sm font-Lexend bg-purple-800 px-2 py-1 rounded-md">hi@weburetech.com</span>
                                 </div> 
                             </div>       
                         </div>
