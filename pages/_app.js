59acbef86480836c297bda307334878c5e769aca
weburetec <58927451+weburetec@users.noreply.github.com>
2023-05-24 17:42:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -34,14 +34,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <>
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} strategy='afterInteractive' />
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive' />
         <Script id="google-analytics" strategy='afterInteractive'>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
           `}
         </Script>
         <Component {...pageProps} />
