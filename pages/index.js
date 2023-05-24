4c02e96f46612a3165c8c61b555e4a78fab883bd
weburetec <58927451+weburetec@users.noreply.github.com>
2023-05-24 19:27:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,22 +33,12 @@ export default function Home() {
   return (
     <>
       <Head>
-       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-        <Script strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `}
-       </Script>
         <title>Webure Technologies - Global Full Service Agency</title>
         <meta
           name="description"
           content="Full service agency for complete IT Solution"
         />
+        <meta name="google-site-verification" content="qEhm8fazg24i0-X_AyqYDjLn-gLGWioqvW44ad_kySw" />    
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
