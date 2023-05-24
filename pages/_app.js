2edc8063f9ceb2b2d2a802a82b16f18a33cd465d
weburetec <58927451+weburetec@users.noreply.github.com>
2023-05-24 17:23:47 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,9 @@ import { ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import Script from 'next/script';
-import Router from 'next/router';
-import * as gtag from '../lib/gtag'
+import {useEffect} from 'react';
+import {useRouter} from 'next/router';
+import * as gtag from '../lib/gtag';
 
 import { store } from "../store";
 
@@ -18,6 +19,18 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_APP_BASEURL;
 axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }) {
+    const router = useRouter()
+
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        ga.pageview(url)
+      }
+
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }, [router.events])
   return (
     <Provider store={store}>
       <>
