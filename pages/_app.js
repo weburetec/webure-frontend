c03690119ab702d451f4771786a50322749faae8
weburetec <58927451+weburetec@users.noreply.github.com>
2023-05-25 23:55:02 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -45,19 +45,9 @@ function MyApp({ Component, pageProps }) {
             gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
           `}
         </Script>
-        <script>
-        {`
-  window.intercomSettings = {
-    api_base: "https://api-iam.intercom.io",
-    app_id: "tc2t1dfe"
-  };
-  `}
-</script>
-
-<script>
-{`
-(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/tc2t1dfe';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-`}
+        <script
+src='//in.fw-cdn.com/30673666/364679.js'
+chat='true'>
 </script>
         <Component {...pageProps} />
       </>
