7fd1224e67639bef85fbd174d5814d9171e64d7a
weburetec <58927451+weburetec@users.noreply.github.com>
2023-05-30 18:17:45 +0530

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -31,9 +31,9 @@ export default function Hero() {
               </p>
               <br />
               <br />
-              <h1 className="banner_heading font-Lexend text-2xl px-20">
+              <h6 className="banner_heading font-Lexend text-2xl px-20">
                 <span style={{ color: "#581C87" }}>CONSULT | CREATE | COLLABORATE</span>
-              </h1>
+              </h6>
               <p className="lead font-Lexend text-2xl leading-5 px-20">
                 Either you are a Start-up, Scale-up, or a Big Company - Webure Technologies is always your Best Tech-Partner!
               </p>
