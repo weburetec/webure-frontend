33a585ed71cec64cccfa94c9cffad7e9b4d585e1
weburetec <58927451+weburetec@users.noreply.github.com>
2023-07-31 14:05:20 +0530

    Update model.js

--- components/engagement/model.js
+++ components/engagement/model.js
@@ -46,6 +46,37 @@ export default function EngageModels(props) {
             </div>
           </div>
         </div>
+        <div>
+          <div className='relative'>
+            <div className="max-w-screen-xl bg-white dark:bg-gray-800 mx-auto relative px-30">
+              <div className='justify-center items-center content-center'>
+                <div className='items-center justify-center py-6 md:justify-center md:space-x-10'>
+                  <div className="flex flex-wrap justify-center -mx-4">
+                    <div className="w-full md:w-1/2 xl:w-1/2 px-1">
+                      <div className="w-full mx-auto mb-10">
+                        <div className="relative rounded-lg overflow-hidden">
+                          <div className='justify-center items-center content-center'>
+                            <div className='ml-5 justify-center items-center content-center col-span-2'>
+                              <p className='font-Lexend font-bold text-xl text-start ml-10'>Hybrid Engagement Model</p>
+                              <p className="font-Lexend text-base leading-6 text-justify -mt-15">The hybrid engagement model is a combination of Dedicated Development and Fixed Price Model. The Business Analysis (BA) Team works with end customer on Dedicated Development Model in freezing the requirements. BA paid on retainer basis. Following this, a detailed scope document prepared. Based on scope document, price and timelines are provided.</p>
+                              <br />
+                              <p className="font-Lexend text-base leading-6 text-justify"><strong>Project Commencement</strong> – If the quote is agreeable to you, we start with the project development as per the agreement. For any changes during the development process, there is a standard change procedure that is handled through a change control mechanism. The project is divided into multiple phases for ease of monitoring by the client. Payments are also phased out accordingly.</p>
+                              <br />
+                              <p className="font-Lexend text-base leading-6 text-justify"><strong>Project Completion</strong> – Once the project is completed, our QA team tests its features and functionalities. After your approval on the same and the final payment, we transfer files to your server and close the project.</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="w-full md:w-1/2 xl:w-1/2 px-1">
+                      <image src="https://www.weburetech.com/static/about-img-6-429b32c76fc0e486eda635ad9c82e84b.webp" alt="Webure Digital Transformation"/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>    
         <div className='relative -mt-12'>
           <div className="max-w-screen-xl bg-white dark:bg-gray-800 mx-auto relative px-30">
             <div className='justify-center items-center content-center'>
@@ -55,14 +86,14 @@ export default function EngageModels(props) {
                     <image src="https://res.cloudinary.com/weburetec/image/upload/v1661625213/digital_transformation_yiqxsf.svg" alt="Webure Digital Transformation"/>                    
                   </div>
                   <div className='col-lg-6 pt-5'>
-                    <p className='font-Lexend font-bold text-xl text-start'>Dedicated Development Facility Model</p>
+                    <p className='font-Lexend font-bold text-xl text-start'>Offshore Delivery Model</p>
                     <image className="-mt-2" src="https://res.cloudinary.com/weburetec/image/upload/v1661627664/LineDesign_e1wjte.svg" sizes="(max-width: 90px) 100vw, 47px" alt="Webure Digital Transformation"/>                    
                     <br />
-                    <p className="font-Lexend text-base leading-6 text-justify -mt-15">The dedicated development facility model  is an excellent solution if you need a complete control over human and technical resources involved into the development process. DTM features a high level of transparency, security, flexibility and scalability.</p>
+                    <p className="font-Lexend text-base leading-6 text-justify -mt-15">The offshore delivery model entails that all project development, from initial study to testing, is done at Advantal’s premises. This particular model works well in scenarios where the project goals are well defined and deviation from the same is unlikely.</p>
                     <br />
-                    <p className="font-Lexend text-base leading-6 text-justify">Dedicated facility will be your extension of engineering / development center. Through such dedicated facilities, we offer the best skill sets, resources, flexibility and time-to-market advantages to the customers, taking all necessary efforts to protect the Intellectual Property Rights (IPR) of the customers. All rights to the IPR developed in the Development Facility vests with the Customer.</p>
+                    <p className="font-Lexend text-base leading-6 text-justify">Management and communication during the course of the project is the responsibility of our program managers – the internal clients, who work with tried and tested methodologies to ensure that delivery is as per schedule.</p>
                     <br />
-                    <p className="font-Lexend text-base leading-6 text-justify">Dedicated Facility also provides a cost effective solution to customers requiring continuous offshore support. For such dedicated relationships, we also implement customer specific quality/process frameworks and teams are trained for the specific needs of a Client. Resources are paid on a retainer basis which is per month billing for resource.</p>
+                    <p className="font-Lexend text-base leading-6 text-justify">Offshore dedicated project team, works within predefined processes, thereby making this model the most cost effective.</p>
                   </div>
                 </div>
               </div>
