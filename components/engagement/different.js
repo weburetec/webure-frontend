cb83673e36659ef3c609a4a76230de49a64eefd6
weburetec <58927451+weburetec@users.noreply.github.com>
2023-07-31 16:36:35 +0530

    Update different.js

--- components/engagement/different.js
+++ components/engagement/different.js
@@ -20,30 +20,34 @@ export default function Different() {
             <div className="flex">
               <div className="col-md-4">
                 <div>
-                  <span>
-                    <image className="content-center text-center" src="https://res.cloudinary.com/ds8lbdapq/image/upload/v1677098839/finans_pi8gjb.png" alt="Microfinance and NBFCs" />
-                  </span>
+                  <img alt="" srcSet="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=64&amp;q=75  1x, https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=128&amp;q=75 2x" src="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&w=64&q=75" width="56" height="56" decoding="async" data-nimg="future" className="h-14 w-14 object-cover" loading="lazy"/>
                   <p className="font-Lexend text-base leading-6 text-justify">Owing to the rise in global customers, brands and competition to sustain, companies of every industry must strengthen themselves by being innovative, technologically updated in order to serve its customers in the best way. Serving its customers in multiple languages, 24 * 7 * 365 customer service, and various time zones and contact channels has become a mandate for any company to sustain the field.<br />Webure, supported by its Technology, Analytical power and Process Excellence methodologies, caters to outsourcing needs of vast array of industries with customized solutions to arrive at better and fruitful results.</p>
                 </div>
               </div>
               <div className="col-md-4">
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
                   <span className="img-containercy">
-                    <img src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png" alt="service-icon5" style="width:87px" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=44&amp;name=service-icon5.png 44w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png 87w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=131&amp;name=service-icon5.png 131w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=174&amp;name=service-icon5.png 174w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=218&amp;name=service-icon5.png 218w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=261&amp;name=service-icon5.png 261w" sizes="(max-width: 87px) 100vw, 87px" />
+                    <div className="overflow-hidden rounded-full bg-slate-50">
+                      <img alt="" srcSet="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=64&amp;q=75  1x, https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=128&amp;q=75 2x" src="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&w=64&q=75" width="56" height="56" decoding="async" data-nimg="future" className="h-14 w-14 object-cover" loading="lazy"/>
+                    </div>
                   </span>
                   <p className="font-Lexend font-medium text-purple-900 text-lg text-left px-5">Unprecedented Quality</p>
                   <p className="font-Lexend text-sm text-purple-900 text-justify px-5">Deploying high-capability software practices for various industries, we ensure the highest quality results and maintenance support, which deliver you an improved return on investment.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
                   <span className="img-containercy">
-                    <img src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png" alt="service-icon5" style="width:87px" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=44&amp;name=service-icon5.png 44w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png 87w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=131&amp;name=service-icon5.png 131w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=174&amp;name=service-icon5.png 174w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=218&amp;name=service-icon5.png 218w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=261&amp;name=service-icon5.png 261w" sizes="(max-width: 87px) 100vw, 87px" />
+                    <div className="overflow-hidden rounded-full bg-slate-50">
+                      <img alt="" srcSet="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=64&amp;q=75  1x, https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=128&amp;q=75 2x" src="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&w=64&q=75" width="56" height="56" decoding="async" data-nimg="future" className="h-14 w-14 object-cover" loading="lazy"/>
+                    </div>
                   </span>
                   <p className="font-Lexend font-medium text-purple-900 text-lg text-left px-5">Project Analytical Skills</p>
                   <p className="font-Lexend text-sm text-purple-900 text-justify px-5">Whether you want us to create something from scratch or modernize your digital product with product engineering, our experts will scrutinize each step with logical data, and realistic timescales to keep you informed.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
                   <span className="img-containercy">
-                    <img src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png" alt="service-icon5" style="width:87px" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=44&amp;name=service-icon5.png 44w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png 87w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=131&amp;name=service-icon5.png 131w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=174&amp;name=service-icon5.png 174w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=218&amp;name=service-icon5.png 218w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=261&amp;name=service-icon5.png 261w" sizes="(max-width: 87px) 100vw, 87px" />
+                    <div className="overflow-hidden rounded-full bg-slate-50">
+                      <img alt="" srcSet="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=64&amp;q=75  1x, https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=128&amp;q=75 2x" src="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&w=64&q=75" width="56" height="56" decoding="async" data-nimg="future" className="h-14 w-14 object-cover" loading="lazy"/>
+                    </div>
                   </span>
                   <p className="font-Lexend font-medium text-purple-900 text-lg text-left px-5">Result Driven Approach</p>
                   <p className="font-Lexend text-sm text-purple-900 text-justify px-5">Providing complete satisfaction with extended business hours and project transparency, we make everything possible to achieve the best results as per the global standards.</p>
@@ -52,21 +56,27 @@ export default function Different() {
               <div className="col-md-4">
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
                   <span className="img-containercy">
-                    <img src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png" alt="service-icon5" style="width:87px" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=44&amp;name=service-icon5.png 44w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png 87w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=131&amp;name=service-icon5.png 131w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=174&amp;name=service-icon5.png 174w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=218&amp;name=service-icon5.png 218w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=261&amp;name=service-icon5.png 261w" sizes="(max-width: 87px) 100vw, 87px" />
+                    <div className="overflow-hidden rounded-full bg-slate-50">
+                      <img alt="" srcSet="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=64&amp;q=75  1x, https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=128&amp;q=75 2x" src="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&w=64&q=75" width="56" height="56" decoding="async" data-nimg="future" className="h-14 w-14 object-cover" loading="lazy"/>
+                    </div>
                   </span>
                   <p className="font-Lexend font-medium text-purple-900 text-lg text-left px-5">Cost Effectiveness</p>
                   <p className="font-Lexend text-sm text-purple-900 text-justify px-5">We are among the most cost-efficient sources for quality-rich IT services, known for professionalism and round-o-clock support. From IT outsourcing to automated systems, we do it all at competitive pricing.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
                   <span className="img-containercy">
-                    <img src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png" alt="service-icon5" style="width:87px" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=44&amp;name=service-icon5.png 44w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png 87w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=131&amp;name=service-icon5.png 131w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=174&amp;name=service-icon5.png 174w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=218&amp;name=service-icon5.png 218w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=261&amp;name=service-icon5.png 261w" sizes="(max-width: 87px) 100vw, 87px" />
+                    <div className="overflow-hidden rounded-full bg-slate-50">
+                      <img alt="" srcSet="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=64&amp;q=75  1x, https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=128&amp;q=75 2x" src="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&w=64&q=75" width="56" height="56" decoding="async" data-nimg="future" className="h-14 w-14 object-cover" loading="lazy"/>
+                    </div>
                   </span>
                   <p className="font-Lexend font-medium text-purple-900 text-lg text-left px-5">Teamwork is our Strength</p>
                   <p className="font-Lexend text-sm text-purple-900 text-justify px-5">Behind every successful project, there is a team that works for hours without getting afraid of obstacles. Team Advantal is on a mission together to transform businesses with a tech-driven and futuristic path.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
                   <span className="img-containercy">
-                    <img src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png" alt="service-icon5" style="width:87px" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=44&amp;name=service-icon5.png 44w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=87&amp;name=service-icon5.png 87w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=131&amp;name=service-icon5.png 131w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=174&amp;name=service-icon5.png 174w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=218&amp;name=service-icon5.png 218w, https://www.classicinformatics.com/hs-fs/hubfs/service-icon5.png?width=261&amp;name=service-icon5.png 261w" sizes="(max-width: 87px) 100vw, 87px" />
+                    <div className="overflow-hidden rounded-full bg-slate-50">
+                      <img alt="" srcSet="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=64&amp;q=75  1x, https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&amp;w=128&amp;q=75 2x" src="https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar-1.c78616b7.png&w=64&q=75" width="56" height="56" decoding="async" data-nimg="future" className="h-14 w-14 object-cover" loading="lazy"/>
+                    </div>
                   </span>
                   <p className="font-Lexend font-medium text-purple-900 text-lg text-left px-5">Fastest Response Time</p>
                   <p className="font-Lexend text-sm text-purple-900 text-justify px-5">When it comes to our support & maintenance assistance, we offer the fastest technical support covering all time zones with post delivery maintenance and project consultation.</p>
