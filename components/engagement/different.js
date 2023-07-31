0fafaee7fe2c1a14356764400c7e75372f77e7d2
weburetec <58927451+weburetec@users.noreply.github.com>
2023-07-31 17:14:25 +0530

    Update different.js

--- components/engagement/different.js
+++ components/engagement/different.js
@@ -25,34 +25,34 @@ export default function Different() {
               </div>
               <div className="col-md-4">
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
-                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" width="80" height="80" decoding="async" data-nimg="future" loading="lazy" />
+                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" decoding="async" data-nimg="future" loading="lazy" />
                   <p className="font-Lexend font-medium text-lg text-left px-5">Unprecedented Quality</p>
                   <p className="font-Lexend text-sm text-justify px-5">Deploying high-capability software practices for various industries, we ensure the highest quality results and maintenance support, which deliver you an improved return on investment.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
-                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" width="80" height="80" decoding="async" data-nimg="future" loading="lazy" />
+                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" decoding="async" data-nimg="future" loading="lazy" />
                   <p className="font-Lexend font-medium text-lg text-left px-5">Project Analytical Skills</p>
                   <p className="font-Lexend text-sm text-justify px-5">Whether you want us to create something from scratch or modernize your digital product with product engineering, our experts will scrutinize each step with logical data, and realistic timescales to keep you informed.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
-                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" width="80" height="80" decoding="async" data-nimg="future" loading="lazy" />
+                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" decoding="async" data-nimg="future" loading="lazy" />
                   <p className="font-Lexend font-medium text-lg text-left px-5">Result Driven Approach</p>
                   <p className="font-Lexend text-sm text-justify px-5">Providing complete satisfaction with extended business hours and project transparency, we make everything possible to achieve the best results as per the global standards.</p>
                 </div>  
               </div>
               <div className="col-md-4">
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
-                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" width="80" height="80" decoding="async" data-nimg="future" loading="lazy" />
+                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" decoding="async" data-nimg="future" loading="lazy" />
                   <p className="font-Lexend font-medium text-lg text-left px-5">Cost Effectiveness</p>
                   <p className="font-Lexend text-sm text-justify px-5">We are among the most cost-efficient sources for quality-rich IT services, known for professionalism and round-o-clock support. From IT outsourcing to automated systems, we do it all at competitive pricing.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
-                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" width="80" height="80" decoding="async" data-nimg="future" loading="lazy" />
+                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" decoding="async" data-nimg="future" loading="lazy" />
                   <p className="font-Lexend font-medium text-lg text-left px-5">Teamwork is our Strength</p>
                   <p className="font-Lexend text-sm text-justify px-5">Behind every successful project, there is a team that works for hours without getting afraid of obstacles. Team Advantal is on a mission together to transform businesses with a tech-driven and futuristic path.</p>
                 </div>
                 <div className="ind_item_1 border-purple-900 hover:border-purple-800">
-                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" width="80" height="80" decoding="async" data-nimg="future" loading="lazy" />
+                  <img className="px-5" alt="" srcset="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" src="https://www.classicinformatics.com/hs-fs/hubfs/service-icon2.png?width=87&amp;name=service-icon2.png" decoding="async" data-nimg="future" loading="lazy" />
                   <p className="font-Lexend font-medium text-lg text-left px-5">Fastest Response Time</p>
                   <p className="font-Lexend text-sm text-justify px-5">When it comes to our support & maintenance assistance, we offer the fastest technical support covering all time zones with post delivery maintenance and project consultation.</p>
                 </div>  
